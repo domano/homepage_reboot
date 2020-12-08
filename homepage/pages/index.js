ababd8d8efb8d2ab0fba45b7918363bf06e99df2
domano <angor.mail+github@googlemail.com>
2020-12-08 21:26:08 +0100

    WIP

--- homepage/pages/index.js
+++ homepage/pages/index.js
@@ -1,6 +1,15 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Image from 'next/image'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faHandsHelping} from "@fortawesome/free-solid-svg-icons"
+
+function Bubble({text, icon}) {
+    return <div className="flex flex-col bg-primary rounded-2xl p-2 border-2 shadow ">
+        <div className="flex items-center justify-center"><FontAwesomeIcon className="text-5xl text-highlight" icon={faHandsHelping}/></div>
+        <p className="text-highlight justify-center items-center text-3xl">{text}</p>
+    </div>
+}
 
 export default function Home() {
     return (
@@ -14,7 +23,8 @@ export default function Home() {
                         width={1500}
                         height={1000}
                     />
-                    <div className="absolute right-0 top-1/4 p-8 flex flex-col bg-primary-light bg-opacity-50 rounded-l-full border-2">
+                    <div
+                        className="absolute right-0 top-1/4 p-8 flex flex-col bg-primary-light bg-opacity-50 rounded-l-full border-2">
                         <p className="xl:text-7xl text-5xl p-8 text-center">
                             Hi! <br/>
                             Ich bin Dino.
@@ -23,10 +33,15 @@ export default function Home() {
                     </div>
 
                 </div>
+                <div className="flex flex-nowrap justify-evenly">
+                    <Bubble text={"IT-Consultant"}/>
+                    <Bubble text={"Systems Architect"}/>
+                    <Bubble text={"Product Owner"}/>
+                    <Bubble text={"Software Developer"}/>
+                    <Bubble text={"Trainer"}/>
+                </div>
                 <p className="p-8 text-3xl">
 
-                    <p className="bg-primary">IT-Consultant</p>, Systemarchitekt, Product Owner, Entwickler und Trainer.
-                    Ich liebe es komplexe Probleme mit Technologie zu lösen.
                 </p>
             </div>
         </div>
--- homepage/tailwind.config.js
+++ homepage/tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
       'primary': '#4e2b4b',
       'primary-light': '#8a5689',
       'secondary': '#96af4c',
-      'danger': '#e4dce7',
+      'highlight': '#e4dce7',
     }),
   },
   variants: {
